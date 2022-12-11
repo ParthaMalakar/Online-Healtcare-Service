c04b286768e9fab3e07b9e158f6ebe5f9ed63a7d
Your Name <you@example.com>
2022-12-12 02:06:06 +0600

    Updated by Farhan

---
+++ Online_Healthcare_Service/DAL/DataAccessFactory.cs
@@ -0,0 +1,57 @@
+﻿using DAL.EF;
+using DAL.Interfaces;
+using DAL.Repos;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DataAccessFactory
+    {
+        public static IRepo<Doctor, int, Doctor> DoctorDataAccess()
+        {
+            return new DoctorRepo();
+        }
+        public static IRepo<Patient, int, Patient> PatientDataAccess()
+        {
+            return new PatientRepo();
+        }
+        public static IRepo<Appointment, int, Appointment> AppointmentDataAccess()
+        {
+            return new AppointmentRepo();
+        }
+
+        public static IRepo<Donar_Info, int, Donar_Info> DonorInfoDataAccess()
+        {
+            return new Donor_InfoRepo();
+        }
+
+        public static IRepo<Blood_Bank, int, Blood_Bank> BloodBankDataAccess()
+        {
+            return new Blood_BankRepo();
+        }
+
+        public static IRepo<Hospital, int, Hospital> HospitalDataAccess()
+        {
+            return new HospitalRepo();
+        }
+        public static IRepo<Ambulance, int, Ambulance> AmbulanceDataAccess()
+        {
+            return new AmbulanceRepo();
+        }
+        public static IRepo<Prescription, int, Prescription> PrescriptionDataAccess()
+        {
+            return new PrescriptionRepo();
+        }
+        public static IRepo<Donate_Money, int, Donate_Money> DonateMoneyDataAccess()
+        {
+            return new DonateMoneyRepo();
+        }
+
+    }
+}
